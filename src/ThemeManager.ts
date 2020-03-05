36e429b4b0ca2b23d6b40f1f8354b85c785d777d
Lucas Martín Segurado <segurado1996@gmail.com>
2020-03-05 11:27:53 -0300

    Minor changes

--- src/ThemeManager.ts
+++ src/ThemeManager.ts
@@ -1,17 +1,24 @@
-import {Theme, DocumentTheme} from "./Types";
+import { Theme, DocumentTheme } from "./Types";
+
+export interface OnThemeChangeFunction {
+    (newTheme: Theme): void;
+}
 
 export default class ThemeManager {
-    constructor() {
+    private onThemeChange: OnThemeChangeFunction;
+
+    constructor(onThemeChange: OnThemeChangeFunction) {
         const localStorageResult = localStorage.getItem('theme');
         const theme: Theme = localStorageResult === 'dark' || localStorageResult === 'light' || localStorageResult === 'system' ? localStorageResult : 'system';
+
+        this.onThemeChange = onThemeChange;
         this.setTheme(theme);
 
-        const self = this;
         new MutationObserver(function (mutations) {
             mutations.forEach(function (mutation) {
                 if (mutation.type == "attributes") {
                     if (mutation.attributeName === 'theme' || mutation.attributeName === 'use-system-preferred-theme') {
-                        localStorage.setItem('theme', self.getTheme());
+                        localStorage.setItem('theme', ThemeManager.getTheme());
                     }
                 }
             });
@@ -20,29 +27,30 @@ export default class ThemeManager {
         })
     }
 
-    setTheme(theme: Theme) {
+    public setTheme(theme: Theme) {
         if (theme === 'system') {
-            document.documentElement.setAttribute('theme', this.getSystemPreferenceTheme());
+            document.documentElement.setAttribute('theme', ThemeManager.getSystemPreferenceTheme());
             document.documentElement.setAttribute('use-system-preferred-theme', 'enabled');
         } else {
             document.documentElement.setAttribute('theme', theme);
             document.documentElement.setAttribute('use-system-preferred-theme', 'disabled');
         }
+        this.onThemeChange(theme);
     }
 
-    getTheme(): Theme {
+    public static getTheme(): Theme {
         return this.isUsingSystemPreferences() ? 'system' : this.getDocumentTheme();
     }
 
-    getDocumentTheme(): DocumentTheme {
+    public static getDocumentTheme(): DocumentTheme {
         return document.documentElement.getAttribute('theme') === 'dark' ? 'dark' : 'light';
     }
 
-    private getSystemPreferenceTheme(): DocumentTheme {
+    public static getSystemPreferenceTheme(): DocumentTheme {
         return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
     }
 
-    private isUsingSystemPreferences(): boolean {
+    public static isUsingSystemPreferences(): boolean {
         return document.documentElement.getAttribute('use-system-preferred-theme') === 'enabled';
     }
 }
